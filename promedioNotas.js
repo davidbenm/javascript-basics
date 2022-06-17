9f5be73cb29ee4ef89becf4d5e2bd49ff3f04c95
davidbenm <99294377+davidbenm@users.noreply.github.com>
2022-06-17 15:20:16 -0400

    Add some more scripts

---
+++ promedioNotas.js
@@ -0,0 +1,20 @@
+const notas = [6, 8, 9, 2, 5, 10];
+let suma = 0;
+
+function sumaNotas(conjuntoASumar) {
+    for (let i = 0; i < conjuntoASumar.length; i++) {
+        suma = suma + conjuntoASumar[i];    
+    }
+    return suma
+}
+
+function promedioNotas() {
+    let promedio = suma / notas.length;
+    return promedio.toFixed(2) // redondeado el promedio a dos lugares decimales
+}
+
+suma = sumaNotas(notas);
+promedio = promedioNotas();
+
+console.log("La suma de las notas es: ", suma);
+console.log("El promedio de las notas es: ", promedio);
